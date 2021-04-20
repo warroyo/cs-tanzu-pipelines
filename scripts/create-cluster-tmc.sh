20872b386c0798eaa9fff9670f5bcce0721de647
warroyo <warroyo7199008@gmail.com>
2021-04-20 14:13:49 -0600

    fix script

--- scripts/create-cluster-tmc.sh
+++ scripts/create-cluster-tmc.sh
@@ -3,18 +3,16 @@
 export TMC_API_TOKEN=$1
 tmc login --no-configure --name cs-gitops
 
-echo "creating cluster"
-
 
 
 RESULT=$(tmc cluster create -f $CLUSTER_YAML 2>&1)
 CREATE_ERROR=$?
 
-if [ CREATE_ERROR -eq 1 ] && [[ "${RESULT}" == *"AlreadyExists"* ]]; then
+if [ $CREATE_ERROR -eq 1 ] && [[ "${RESULT}" == *"AlreadyExists"* ]]; then
 echo "cluster exists updating..."
 tmc cluster update -f $CLUSTER_YAML
 
-elif [ CREATE_ERROR -eq 1 ]; then
+elif [ $CREATE_ERROR -eq 1 ]; then
 echo "there was an error"
 echo $RESULT
 exit 1
