9be1f956dca1e6ff5e8259100ad8ea9657f2ae33
warroyo <warroyo7199008@gmail.com>
2021-04-21 16:29:53 -0600

    fix dir

--- scripts/login-tkc.sh
+++ scripts/login-tkc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 KUBECTL_PATH=/usr/bin/kubectl
+mkdir -p ~/.kube
 tmc cluster auth admin-kubeconfig get ${CLUSTER_NAME} -p ${PROVISIONER} -m ${MGMT_CLUSTER} > ~/.kube/config
 
 ${KUBECTL_PATH} config use-context ${CLUSTER_NAME}
