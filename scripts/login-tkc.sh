1df9071faad6c7590c2184164b70917e92550b76
warroyo <warroyo7199008@gmail.com>
2021-04-22 10:51:14 -0600

    fix login

--- scripts/login-tkc.sh
+++ scripts/login-tkc.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export TMC_API_TOKEN=$1
 tmc login --no-configure --name cs-gitops
 KUBECTL_PATH=/usr/bin/kubectl
 tmc cluster auth admin-kubeconfig get ${CLUSTER_NAME} -p ${PROVISIONER} -m ${MGMT_CLUSTER} > /tmp/${CLUSTER_NAME}-config.yml
