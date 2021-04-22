92de36b3956e3f4bede43f3679933be4ded3eb6e
warroyo <warroyo7199008@gmail.com>
2021-04-22 08:36:17 -0600

    adding login

--- scripts/login-tkc.sh
+++ scripts/login-tkc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+export TMC_API_TOKEN=$1
+tmc login --no-configure --name cs-gitops
 KUBECTL_PATH=/usr/bin/kubectl
 mkdir -p ~/.kube
 tmc cluster auth admin-kubeconfig get ${CLUSTER_NAME} -p ${PROVISIONER} -m ${MGMT_CLUSTER} > ~/.kube/config
